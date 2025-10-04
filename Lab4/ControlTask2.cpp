bd1995292584a10875f511d820d729d068f4e4fc
Shvalev Anton <shvalev_as88@mail.ru>
2025-10-04 23:52:36 +0300

    add control task 2

--- Lab4/ControlTask2.cpp
+++ Lab4/ControlTask2.cpp
@@ -2,12 +2,31 @@
 #include <Windows.h>
 using namespace std;
 
+bool Input(int& a, int& b) {
+   // Проверка на число 
+   if (!(cin >> a)) {
+      return false;
+   }
+
+   // Проверка на число
+   if (!(cin >> b)) {
+      return false;
+   }
+
+   return true;
+}
 
 int ControlTask2()
 {
    SetConsoleOutputCP(1251);
    SetConsoleCP(1251);
 
+   int a, b;
+   if (!Input(a, b)) {
+      cerr << "error";
+      return 1;
+   }
+   int s = a + b;
 
    return 0;
 }
