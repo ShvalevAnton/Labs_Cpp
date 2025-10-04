17cf54e952eac99b058c8bc49dab6c6224faee83
Shvalev Anton <shvalev_as88@mail.ru>
2025-10-04 23:25:56 +0300

    add exercise 2

---
+++ Lab4/Exercise2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void swap(int*, int*);
+void swap(int&, int&);
+
+void swap(int* x, int* y)
+{
+   int temp;
+   temp = *x;
+   *x = *y;
+   *y = temp;
+}
+void swap(int& x, int& y)
+{
+   int temp;
+   temp = x;
+   x = y;
+   y = temp;
+}
+
+void print(int x, int y)
+{
+   cout << "x = " << x << "; y = " << y << endl;
+}
+
+int Exercise2()
+{
+   SetConsoleOutputCP(1251);
+   SetConsoleCP(1251);
+
+   int x = 5, y = 10;
+
+   swap(&x, &y);
+   print(x, y);
+
+   swap(x, y);
+   print(x, y);
+   
+
+   return 0;
+}
+
