9784e89015ed349d727c479373ceef5ffd525696
Shvalev Anton <shvalev_as88@mail.ru>
2025-10-04 23:18:44 +0300

    add exercise 1

---
+++ Lab4/Exercises.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef EXERCISES_H
+#define EXERCISES_H
+
+int Exercise1();
+int Exercise2();
+int ControlTask1();
+int ControlTask2();
+int ControlTask3();
+int ControlTask4();
+int ControlTask5();
+
+
+#endif
---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,17 @@
+﻿#include "Exercises.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   // Упражнение 1. Передача параметров
+   Exercise1();
+   // Упражнение 2. Реализация функции обмена значений
+   //Exercise2();
+   
+
+   // Контрольные задания
+
+}
+
