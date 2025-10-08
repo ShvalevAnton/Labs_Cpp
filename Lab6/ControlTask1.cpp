270a3784e5cb2f0dd5ffc5976c9c74d60ffd2961
Shvalev Anton <shvalev_as88@mail.ru>
2025-10-08 22:44:02 +0300

    add control task 1

--- Lab6/ControlTask1.cpp
+++ Lab6/ControlTask1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <Windows.h>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int ControlTask1()
@@ -7,6 +9,28 @@ int ControlTask1()
    SetConsoleOutputCP(1251);
    SetConsoleCP(1251);
 
+   string filename;
+   cout << "Введите имя файла для записи (например, Стихотворение.txt): ";
+   getline(cin, filename);
+
+   ofstream file(filename);
+   if (!file.is_open()) {
+      cerr << "Ошибка: не удалось открыть файл для записи." << endl;
+      return 1;
+   }
+
+   cout << "Введите стихотворение (для окончания ввода введите пустую строку):" << endl;
+   string line;
+   while (true) {
+      getline(cin, line);
+      if (line.empty()) {
+         break;
+      }
+      file << line << '\n';
+   }
+
+   file.close();
+   cout << "Стихотворение записано в файл: " << filename << endl;
 
 
    return 0;
