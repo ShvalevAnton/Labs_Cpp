3e68700445e2fea54bf473907a0b1ec7a084e826
Shvalev Anton <shvalev_as88@mail.ru>
2025-10-29 23:25:19 +0300

    Add exercise 3

--- Lab14/Exercise3.cpp
+++ Lab14/Exercise3.cpp
@@ -14,12 +14,14 @@ int Exercise3()
 
 	vector<string> v1{ "one", "two", "three", "four", "five", "six" };
 	vector<int> v2 = { 1, 2, 3, 4, 5, 6 };
-	vector<float> v3 = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
-	auto t1 = std::make_tuple(v1[0], v1[1], v3[0]);
-	auto t2 = std::make_tuple(v1[0], v1[1], v2[1], v3[0], v3[1]);
-	printTuple(t1);
-	printTuple(t2);
-	return 0;
+	vector<double> v3 = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+
+	auto t0 = make_tuple(v1[1], v2[1], v3[1]);
+
+	printTupleOfThree(t0);
+
+	auto t3 = make_tuple(v1[0], v1[1], v2[2]);
+	printTupleOfThree(t3);
 
 	return 0;
 }
