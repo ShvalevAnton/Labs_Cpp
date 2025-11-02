f66965936f22264b71cc8bd8582f6dc5c17b997c
Shvalev Anton <shvalev_as88@mail.ru>
2025-11-02 11:47:46 +0300

    Add exercise 4.

--- Lab14/Exercise4.cpp
+++ Lab14/Exercise4.cpp
@@ -4,24 +4,106 @@
 #include <string>
 #include <tuple>
 #include <vector>
-
+#include <cstdlib>
 using namespace std;
+
+
+
+// Шаблонный класс массива
+template<class T, class T1> class MyArray
+{
+public:
+   MyArray(int size);
+   ~MyArray();
+   T1 sum();
+   T average_value();
+   void show_array();
+   int add_value(T);
+private:
+   T* data;
+   int size;
+   int index;
+};
+
+// Конструктор
+template<class T, class T1>
+MyArray<T, T1>::MyArray(int size)
+{
+   data = new T[size];
+   if (data == nullptr)  // Лучше использовать nullptr вместо NULL в C++
+   {
+      cerr << "Error memory ---- exit program" << endl;
+      exit(1);
+   }
+   this->size = size;    // Используем this-> для избежания неоднозначности
+   this->index = 0;
+}
+
+// Деструктор
+template<class T, class T1>
+MyArray<T, T1>::~MyArray()
+{
+   delete[] data;  // Освобождаем память
+}
+
+// Метод для вычисления суммы
+template<class T, class T1> T1 MyArray<T, T1>::sum()
+{
+   T1 sum = 0;
+   for (int i = 0; i < index; i++) sum += data[i];
+   return(sum);
+}
+
+// Метод для вычисления среднего значения
+template<class T, class T1> T MyArray<T, T1>::average_value()
+{
+   T1 sum_val = 0;
+   for (int i = 0; i < index; i++) sum_val += data[i];
+   return (sum_val / index);
+}
+
+// Метод для отображения массива
+template<class T, class T1> void MyArray<T, T1>::show_array()
+{
+   for (int i = 0; i < index; i++) cout << data[i] << ' ';
+   cout << endl;
+}
+
+// Метод для добавления значения
+template<class T, class T1> int MyArray<T, T1>::add_value(T value)
+{
+   if (index == size)
+      return(-1);
+   else
+   {
+      data[index] = value;
+      index++;
+      return(0);
+   }
+}
 
 int Exercise4()
 {
-	SetConsoleOutputCP(1251);
-	SetConsoleCP(1251);
+   // Создание массивов разных типов
+   MyArray<int, long> numbers(100);
+   MyArray<float, float> values(200);
 
-	vector<string> v1{ "one", "two", "three", "four", "five", "six" };
-	vector<int> v2 = { 1, 2, 3, 4, 5, 6 };
-	vector<double> v3 = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+   // Заполнение и работа с массивом целых чисел
+   cout << "Array of integers:" << endl;
+   int i;
+   for (i = 0; i < 50; i++) numbers.add_value(i);
+   numbers.show_array();
+   cout << "Sum = " << numbers.sum() << endl;
+   cout << "Average = " << numbers.average_value() << endl;
 
-	auto t0 = make_tuple(v1[1], v2[1], v3[1]);
+   cout << endl;
 
-	printTupleOfThree(t0);
+   // Заполнение и работа с массивом вещественных чисел
+   cout << "Array of floats:" << endl;
+   for (i = 0; i < 100; i++) values.add_value(i * 100);
+   values.show_array();
+   cout << "Sum = " << values.sum() << endl;
+   cout << "Average = " << values.average_value() << endl;
 
-	auto t3 = make_tuple(v1[0], v1[1], v2[2]);
-	printTupleOfThree(t3);
-
-	return 0;
+   return 0;
 }
--- Lab14/functions.h
+++ Lab14/functions.h
@@ -5,6 +5,7 @@
 #include <tuple>
 #include <vector>
 
+
 using namespace std;
 using Tuple = tuple<string, int, double>;
 
@@ -52,3 +53,69 @@ void printTuple(const tuple<Args...>& t)
    TuplePrinter<decltype(t), sizeof...(Args)>::print(t);
    cout << ")" << endl;
 }
+
+//// Шаблонный класс массива
+//template<class T, class T1> class array
+//{
+//public:
+//   array(int size);
+//   T1 sum();
+//   T average_value();
+//   void show_array();
+//   int add_value(T);
+//private:
+//   T* data;
+//   int size;
+//   int index;
+//};
+//
+//// Конструктор
+//template<class T, class T1> array<T, T1>::array(int size)
+//{
+//   data = new T[size];
+//   if (data == NULL)
+//   {
+//      cerr << "Error memory ---- exit program" << endl;
+//      exit(1);
+//   }
+//   array::size = size;
+//   array::index = 0;
+//}
+//
+//// Метод для вычисления суммы
+//template<class T, class T1> T1 array<T, T1>::sum()
+//{
+//   T1 sum = 0;
+//   for (int i = 0; i < index; i++) sum += data[i];
+//   return(sum);
+//}
+//
+//// Метод для вычисления среднего значения
+//template<class T, class T1> T array<T, T1>::average_value()
+//{
+//   T1 sum_val = 0;
+//   for (int i = 0; i < index; i++) sum_val += data[i];
+//   return (sum_val / index);
+//}
+//
+//// Метод для отображения массива
+//template<class T, class T1> void array<T, T1>::show_array()
+//{
+//   for (int i = 0; i < index; i++) cout << data[i] << ' ';
+//   cout << endl;
+//}
+//
+//// Метод для добавления значения
+//template<class T, class T1> int array<T, T1>::add_value(T value)
+//{
+//   if (index == size)
+//      return(-1);
+//   else
+//   {
+//      data[index] = value;
+//      index++;
+//      return(0);
+//   }
+//}
+//
+
